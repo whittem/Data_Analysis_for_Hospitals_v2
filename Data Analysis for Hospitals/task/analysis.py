8fea431fe0fae359b9a84e34f334271e6caee3b5
info <whittem@gmail.com>
2025-02-16 21:01:30 +0000

    updated 16/02/25
    Signed-off-by: info <whittem@gmail.com>

--- Data Analysis for Hospitals/task/analysis.py
+++ Data Analysis for Hospitals/task/analysis.py
@@ -5,18 +5,11 @@ import pandas as pd
 
 pd.set_option('display.max_columns', 8)
 
+# Function to read csv and amend column names
 def handle_frame(name):
     frame = pd.read_csv(name)
     frame.rename(columns={'HOSPITAL': 'hospital', 'Hospital': 'hospital', 'Sex': 'gender', 'Male/female': 'gender'}, inplace=True)
     return frame
-
-all_data = pd.concat([handle_frame(r'./test//general.csv'), handle_frame(r'./test/prenatal.csv'), handle_frame(r'./test/sports.csv')])
-print(all_data)
-
-#general = pd.read_csv(r'./test//general.csv')
-#prenatal = pd.read_csv(r'./test//prenatal.csv')
-#sports = pd.read_csv(r'./test//sports.csv')
-
 
 # function to calculate % share of given diagnosis and hospital
 def patient_share(hospital, diagnosis):
@@ -25,13 +18,8 @@ def patient_share(hospital, diagnosis):
     x = share_by_hospital.loc[hospital].round(3)
     return (x)
 
-
-# Change column names
-#prenatal.rename(columns={'HOSPITAL': 'hospital', 'Sex': 'gender'}, inplace=True)
-#sports.rename(columns={'Hospital': 'hospital', 'Male/female': 'gender'}, inplace=True)
-
-# Combine the dataframes
-#all_data = pd.concat([general, prenatal, sports], ignore_index=True)
+# Combine the csv files and call handle_frame function to amend column names
+all_data = pd.concat([handle_frame(r'./test//general.csv'), handle_frame(r'./test/prenatal.csv'), handle_frame(r'./test/sports.csv')])
 
 # Drop unnamed columns
 all_data.drop(columns=['Unnamed: 0'], inplace=True)
@@ -43,18 +31,15 @@ all_data.dropna(how='all', inplace=True)
 all_data['gender'] = all_data['gender'].astype(str).str.lower().str.strip().map(
     lambda x: 'm' if x.startswith('m') else 'f')
 
-# Replace the NaN values in the bmi, diagnosis, blood_test, ecg, ultrasound, mri, xray, children, months columns with zeros
-all_data.iloc[:, 6:] = all_data.iloc[:, 6:].fillna(0)
+# Replace the NaN values wih zeros
+all_data.fillna(0, inplace=True)
 
 # Hospital with highest number of patients
 q1 = all_data['hospital'].value_counts().idxmax()
 
 # Share of patients in hosp with issues
-q2 = round(all_data.query("hospital == 'general' and diagnosis == 'stomach'").shape[0] / all_data.query("hospital == 'general'").shape[0], 3)
-print(all_data.shape[0])
-print(f'This is a test of a better way to code q2. Answer = {q2}')
-stomach_share_general = patient_share('general', 'stomach')
-dislocation_share_general = patient_share('sports', 'dislocation')
+q2 = patient_share('general', 'stomach')
+q3 = patient_share('sports', 'dislocation')
 
 # Median difference ages general/sports hospitals
 age_pivot = all_data.pivot_table(index='hospital', values='age', aggfunc='median')
@@ -65,7 +50,7 @@ highest_hospital = all_data[all_data['blood_test'] == 't']['hospital'].value_cou
 highest_count = all_data[all_data['blood_test'] == 't']['hospital'].value_counts().max()
 
 print(f'The answer to the 1st question is {q1}')
-print(f'The answer to the 2nd question is {stomach_share_general}')
-print(f'The answer to the 3rd question is {dislocation_share_general}')
+print(f'The answer to the 2nd question is {q2}')
+print(f'The answer to the 3rd question is {q3}')
 print(f'The answer to the 4th question is {float(diff.iloc[0])}')
 print(f'The answer to the 5th question is {highest_hospital}, {highest_count} blood tests')
